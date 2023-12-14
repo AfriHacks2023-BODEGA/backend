68de5a6e219f05f0f70e25e456755c287c0ef6bf
Oregunwa Oluwadamilola Michael <moregunwa@gmail.com>
2023-12-14 08:31:12 +0100

    Cleaned up the code

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import re
 from flask import Flask, request
 from typing import List, Union
-import re
 
 app = Flask(__name__)
+
 
 class Menu:
     def __init__(self,
@@ -11,14 +12,21 @@ class Menu:
                  options: List["Menu"] = [],
                  default_options: Union["Menu", None] = None,
                  fallthrough: bool = False,
-                 ending: bool = False
+                 ending: bool = False,
+                 show_back: bool = False,
                  ) -> None:
+        self.parent: Union[Menu, None] = None
         self.options = options
+        for option in self.options:
+            option.parent = self
         self.label = label
         self.title = title
         self.default = default_options
+        if self.default != None:
+            self.default.parent = self
         self.fallthrough = fallthrough
         self.ending = ending
+        self.show_back = show_back
 
     def get_item_string(self, text: str) -> str:
         if text == "":
@@ -29,7 +37,7 @@ class Menu:
             if path[0] != "0":
                 menu = self.get_option(int(path[0]))
             else:
-                menu = self
+                menu = self.parent if self.parent != None else self
         except:
             menu = self.get_option(None)
 
@@ -47,7 +55,7 @@ class Menu:
         for index, option in enumerate(self.options):
             string += f"{index+1}. {option.label}\n"
 
-        string += f"{'0. Go back' if len(self.options) > 0 else ''}"
+        string += f"{'0. Go Back' if len(self.options) > 0 or self.show_back else ''}"
         return string
 
 
@@ -67,14 +75,14 @@ def get_order_menu() -> Menu:
                 ])
 
 
-def get_main_menu():
+def get_main_menu(phone_number: str = ""):
     return Menu("", "Welcome to Bodega", [
         Menu("To continue", "What service do you want to use", [
             Menu("Set up your account with us", "Enter your NIN Number",
                 default_options=Menu(
                     "",
                     "Enter your Federal Ministry of Agriculture and Food Security Licence Number",
-                    default_options=Menu("", "Thank you for signing up for the platform", ending=True),),
+                    default_options=Menu("", "Thank you for signing up for the platform", ending=True, ),),
                  ),
             Menu("Get access to the market", "", options=[
                 Menu("Manage Products", "Manage your product", options=[
@@ -83,15 +91,15 @@ def get_main_menu():
                             Menu("Yam")
                         ]),
                     Menu("Add Product", "What product do you want to add",
-                         options=get_items(), fallthrough=True, default_options=Menu("", "You added the product to your product list", ending=True),),
+                         options=get_items(), fallthrough=True, default_options=Menu("", "You added the product to your product list", show_back=True),),
                     Menu("Remove Product", "What product do you want to remove",
-                         options=[get_items()[0]], fallthrough=True, default_options=Menu("", "You removed the product to your product list", ending=True),),
+                         options=[get_items()[0]], fallthrough=True, default_options=Menu("", "You removed the product to your product list", show_back=True),),
                 ]),
                 Menu("See Orders", "Here a list of orders that you have", options=[
                     get_order_menu()
                 ]),
             ]),
-            Menu("Get start up funding and helps", "Choose provider", [
+            Menu("Get start up funding and helps", "Who do you want to get support from", [
                 Menu("Government", "Here are the organizations under the government that have plans", options=[
                     Menu("Federal Ministry of Agriculture and Food Security", "Here are the plans", [
                         Menu("Loan of ₦500,000 for 6 months",),
@@ -100,9 +108,9 @@ def get_main_menu():
                     ], fallthrough=True, default_options=Menu("", "We are processing your request. You will recieve a SMS on the progress application", ending=True),),
                 ]),
                 Menu("Angel Investors",
-                     "Sorry there are no offers right now, We'll inform you of when an offer opens", ending=True),
+                     "Sorry there are no offers right now, We'll inform you of when an offer opens", show_back=True),
                 Menu(
-                    "NGOs", "Sorry there are no offers right now, We'll inform you of when an offer opens", ending=True),
+                    "NGOs", "Sorry there are no offers right now, We'll inform you of when an offer opens", show_back=True),
             ]),
         ])])
 
@@ -110,11 +118,7 @@ def get_main_menu():
 @app.route('/test', methods=['POST', "GET"])
 def test_ussd():
     global repsonse
-    text = request.values.get("text")
-    text = "" if text == None else text
-
-    # The extra replace shit is for if we miss a last *0
-    text = re.sub(r"\*\d+\*0", "", text).replace("*0", "")
+    text = request.values.get("text", "")
 
     return get_main_menu().get_item_string(text)
 
@@ -124,9 +128,8 @@ def ussd_callback():
     global response
     session_id = request.values.get("sessionId", None)
     service_code = request.values.get("serviceCode", None)
-    phone_number = request.values.get("phoneNumber", None)
+    phone_number = request.values.get("phoneNumber", "")
     text = request.values.get("text", "")
-    text = re.sub(r"\*\d+\*0", "", text).replace("*0", "")
 
     return get_main_menu().get_item_string(text)
 
